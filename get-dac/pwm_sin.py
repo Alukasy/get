460dafbb688f80a87a86d76ae276603ec444da6a
Alukasy <alukasy1@inbox.ru>
2026-02-25 14:12:00 +0300

    Update pwm_sin.py

--- get-dac/pwm_sin.py
+++ get-dac/pwm_sin.py
@@ -1,11 +1,11 @@
-import pwm_dac as pwm
 import RPi.GPIO as GPIO
 import signal_generator as sg 
 import time 
 
+# Параметры
 amplitude = 3.0
-signal_frequency = 10 / 2
-sampling_frequency = 1000 / 2 
+signal_frequency = 5.0      # (10 / 2)
+sampling_frequency = 500.0  # (1000 / 2) 
 
 class PWM_DAC:
     def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose = False):
@@ -14,6 +14,7 @@ class PWM_DAC:
         self.dynamic_range = dynamic_range
         self.verbose = verbose
 
+        GPIO.setwarnings(False) # Чтобы не спамило варнингами при перезапуске
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.gpio_pin, GPIO.OUT)
 
@@ -25,28 +26,39 @@ class PWM_DAC:
         GPIO.cleanup()
     
     def set_voltage(self, voltage):
-        if not (0.0 <= voltage <= self.dynamic_range):
-            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
-            print("Устанавливаем 0.0 В")
-            voltage = 0.0
+        # Ограничиваем напряжение, чтобы не выйти за 0..3.3
+        if voltage < 0: voltage = 0
+        if voltage > self.dynamic_range: voltage = self.dynamic_range
 
-        duty_cycle = (voltage/ self.dynamic_range) * 100
+        duty_cycle = (voltage / self.dynamic_range) * 100
         self.pwm.ChangeDutyCycle(duty_cycle)
 
+# Инициализация
 dynamic_range = 3.3
 pin = 12
-dac = pwm.PWM_DAC(pin, 1000, dynamic_range)
+# Создаем объект класса, который описан выше
+dac = PWM_DAC(pin, 1000, dynamic_range)
 
 try: 
-    print(f"Частота:{signal_frequency}Hz, Амплитуда:{amplitude}V")
-
+    print(f"Генерация: Частота:{signal_frequency}Hz, Амплитуда:{amplitude}V")
     start_time = time.time()
+    
     while True:
         t = time.time() - start_time
+        
+        # 1. Считаем значение (0..1)
         norm_val = sg.get_sin_wave_amplitude(signal_frequency, t)
+        
+        # 2. Масштабируем (0..3.0)
         target_voltage = norm_val * amplitude
+        
+        # 3. ОТПРАВЛЯЕМ НА ЦАП (этого не было)
+        dac.set_voltage(target_voltage)
+        
+        # 4. Ждем такта дискретизации
         sg.wait_for_sampling_period(sampling_frequency)
+
 except KeyboardInterrupt:
-    print("прервано")
+    print("\nПрервано пользователем")
 finally:
     dac.deinit()
