5ccb6fde321a0deb351b83906a16d4d7fce6b02b
Alukasy <alukasy@inbox.ru>
2026-02-25 14:50:27 +0300

    sdfsdfs

--- get-dac/mcp4725-sin.py
+++ get-dac/mcp4725-sin.py
@@ -1,26 +1,32 @@
+import RPi.GPIO as GPIO
 import signal_generator as sg 
 import time 
-import pwm_dac as pwm
+import mcp4725_driver as pwm
 
-amplitude = 3.2
-signal_frequency = 10
+amplitude = 3.0
+signal_frequency = 10    
 sampling_frequency = 1000 
 
 
 
 dynamic_range = 3.3
 pin = 12
+dac = pwm.MCP4725(5.0)
 
 try: 
-    dac = pwm.PWM_DAC(pin, 1000, dynamic_range, True)
     print(f"Генерация: Частота:{signal_frequency}Hz, Амплитуда:{amplitude}V")
     start_time = time.time()
     
     while True:
         t = time.time() - start_time
+        
         norm_val = sg.get_sin_wave_amplitude(signal_frequency, t)
+        
         target_voltage = norm_val*amplitude
+
         dac.set_voltage(target_voltage)
+        
+
         sg.wait_for_sampling_period(sampling_frequency)
 
 except KeyboardInterrupt:
