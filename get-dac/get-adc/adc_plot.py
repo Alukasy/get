778da9d19a51e469eb4e82be41e168c78b880efe
alukasy <alukasy1@inbox.ru>
2026-03-04 13:47:30 +0300

    adc_graf

---
+++ get-dac/get-adc/adc_plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt 
+
+def plot_voltage_vs_time(time, voltage, max_voltage):
+    plt.figure(figsize = (10,6))
+    plt.plot(time, voltage, label = "U(t)")
+    plt.xlabel("Время)
+    plt.ylabel("Напряжение")
+    plt.xlim(0, max(time) if time else 1)
+    plt.ylim(0, max_voltage+0.5)
+    plt.grid(True)
+    plt.show()
