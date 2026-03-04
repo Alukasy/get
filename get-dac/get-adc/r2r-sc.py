6c9a4317397038c4027d90872428bcce635820b3
Alukasy <alukasy1@inbox.ru>
2026-03-04 14:36:43 +0300

    324

--- get-dac/get-adc/r2r-sc.py
+++ get-dac/get-adc/r2r-sc.py
@@ -1,9 +1,8 @@
 import time
 from r2r_adc import R2R_ADC
-from adc_plot import plot_voltage_vs_time
+from adc_plot import plot_voltage_vs_time, plot_sampling_period_hist
 
 def main():
-    # Параметры эксперимента
     MY_DYNAMIC_RANGE = 3.3
     duration = 3.0
     voltage_values = []
@@ -11,7 +10,6 @@ def main():
     
     adc = None
     try:
-        # Инициализация с быстрым временем сравнения
         adc = R2R_ADC(dynamic_range=MY_DYNAMIC_RANGE, compare_time=0.0001)
         
         start_time = time.time()
@@ -20,7 +18,6 @@ def main():
         while (time.time() - start_time) < duration:
             current_elapsed = time.time() - start_time
             
-            # Измеряем напряжение и фиксируем время
             v = adc.get_sc_voltage()
             
             voltage_values.append(v)
@@ -29,6 +26,7 @@ def main():
         print("Запись завершена. Построение графика...")
         plot_voltage_vs_time(time_values, voltage_values, MY_DYNAMIC_RANGE)
 
+        plot_sampling_period_hist(time_values)
     except KeyboardInterrupt:
         print("\nПрервано пользователем")
     finally:
